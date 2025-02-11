8817ae4caa552b18f73f78a6747dcdaa65506dd6
AmanKhys <amankhys2112@gmail.com>
2025-02-11 12:35:57 +0530

    change the gst_no constraint to account for exact gst_no; change login_otps schema to otps;

--- repository/sql/schemas/schemas.sql
+++ repository/sql/schemas/schemas.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name TEXT NOT NULL CHECK (name ~* '^[a-zA-Z]{3,}[a-zA-Z ]*$'),
-    email TEXT NOT NULL UNIQUE CHECK (email ~* '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$'),
+    email TEXT NOT NULL UNIQUE CHECK (email ~* '^(0[1-9]|1[0-9]|2[0-9]|3[0-5])[A-Z]{5}[0-9]{4}[A-Z][1-9A-Z]Z[0-9A-Z]$'),
     phone BIGINT UNIQUE CHECK (phone >= 1000000000 AND phone <= 9999999999),
     password TEXT NOT NULL,
     role TEXT NOT NULL CHECK (role IN ('user', 'seller', 'admin')),
@@ -77,7 +77,7 @@ CREATE TABLE IF NOT EXISTS reviews (
 );
 
 -- Login OTPs Table
-CREATE TABLE IF NOT EXISTS login_otps (
+CREATE TABLE IF NOT EXISTS otps(
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     otp INTEGER NOT NULL DEFAULT FLOOR(RANDOM() * 999999),
