96f8c224ae4a24e0ecf2b5557f5daf6914f14e94
AmanKhys <amankhys2112@gmail.com>
2025-02-12 00:23:15 +0530

    make mail package; make sendOTPMail() in mail package;

---
+++ pkg/mail/mail.go
@@ -0,0 +1,57 @@
+package mail
+
+import (
+	"fmt"
+	"net/smtp"
+	"time"
+
+	env "github.com/joho/godotenv"
+)
+
+func returnAuth() (smtp.Auth, error) {
+	envM, err := env.Read(".env")
+	if err != nil {
+		return nil, err
+	}
+	identity := envM["smtp_identity"]
+	smtpHost := envM["smtp_host"]
+	smtpMail := envM["smtp_mail"]
+	smtpPassword := envM["smtp_password"]
+	auth := smtp.PlainAuth(identity, smtpMail, smtpPassword, smtpHost)
+	return auth, nil
+}
+
+func SendOTPMail(otp int, expires_at time.Time, recepientMail string) error {
+	envM, err := env.Read(".env")
+	if err != nil {
+		return err
+	}
+	smtpServer := envM["smpt_server"]
+	smtpMail := envM["smpt_mail"]
+
+	auth, err := returnAuth()
+	if err != nil {
+		return err
+	}
+	var recepients []string
+	message := fmt.Sprintf("From: %s\r\n", smtpMail) +
+		"To: " + recepientMail + "\r\n" +
+		"Subject: OTP Verification\r\n" +
+		"MIME-Version: 1.0\r\n" +
+		"Content-Type: text/plain; charset=\"utf-8\"\r\n" +
+		"\r\n" + // Empty line separates headers from body
+		fmt.Sprintf("Dear User,\n\n"+
+			"Your One-Time Password (OTP) for verification is: %d\n\n"+
+			"This OTP will expire on: %s\n"+
+			"Time remaining: %s\n\n"+
+			"Please do not share this OTP with anyone.\n\n"+
+			"If you did not request this, please ignore this email.\n\n"+
+			"Best regards,\nToy Stores Ecom",
+			otp,
+			expires_at.Format("Monday, 02 Jan 2006, 03:04 PM MST"), // More readable date format
+			time.Until(expires_at).Round(time.Second).String())     // Time left rounded to seconds
+
+	recepients = append(recepients, recepientMail)
+	err = smtp.SendMail(smtpServer, auth, smtpMail, recepients, []byte(message))
+	return err
+}
